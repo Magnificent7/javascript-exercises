6edb1a183c62d100ecbd867ba9ccf55a8afe4ae3
dani <dani.zaghian@gmail.com>
2017-06-12 20:08:01 -0700

    conditionals and loops

---
+++ conditionals_and_loops.js
@@ -0,0 +1,104 @@
+// RUBY CONDITIONALS
+// if x > 50 
+//   puts "high number"
+// elsif x < 30
+//   puts "low number"
+// else
+//   puts "medium number"
+// end
+
+// JS CONDITIONALS
+// var x = 10;
+// if (x > 50) { 
+//   console.log("high number");
+// } else if (x < 25) {
+//   console.log("low number");
+// } else {
+//   console.log("medium number");
+// }
+
+// // RUBY EQUALITY CHECK
+// 5 == 5 //true
+// 5 == '5' //false
+// 5 != '5' //true
+
+// JS EQUALITY CHECK
+// console.log(5 == 5); //true
+// console.log(5 == '5'); //true (loose)
+// console.log(5 === '5'); //false (strict)
+// console.log(5 !== '5'); // true (strict)
+// console.log(5 != '5'); // false (loose)
+
+// RUBY FALSEYS
+// puts "WAT" if false
+// puts "WAT" if nil
+
+// JS FALSEYS
+// if (false) {console.log("WAT");}
+// if (null) {console.log("WAT");}
+// if (undefined) {console.log("WAT");}
+// if (0) {console.log("WAT");}
+// if ("") {console.log("WAT");}
+// if (NaN) {console.log("WAT");}
+
+// RUBY RANDOM NUMBER
+// rand(1..10)
+
+// JS RANDOM NUMBER
+// console.log(Math.floor(Math.random() * 10 + 1));
+
+// RUBY WHILE LOOPS
+// i = 0
+// text = ""
+// while i < 10
+//   text = text + "our number is" + i
+//   i += 1
+// end
+// puts text
+
+// JS WHILE LOOPS
+// var i = 0;
+// var text = "";
+// while (i < 10) {
+//   text = text + " our number is " + i;
+//   i++;
+// }
+// console.log(text);
+
+// RUBY FOR LOOPS
+// for i in 0...10
+//   puts i
+// end
+
+// JS FOR LOOPS
+// for (var i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+
+// //logical equivalent with a while loop
+// var i =0;
+// while (i < 10) {
+//   console.log(i);
+//   i++;
+// }
+
+// RUBY EACH ARRAY
+// fruits = ['apple', 'banana', 'fig', 'plum', 'kumquat', 'grape']
+// fruits.each do |fruit|
+//   puts fruit
+// end
+
+// JS EACH ARRAY
+// var fruits = ['apple', 'banana', 'fig', 'plum', 'kumquat', 'grape', 'kiwi'];
+// for (var i = 0; i < fruits.length; i++) {
+//   console.log(fruits[i]);
+// }
+
+
+
+
+
+
+
+
+
