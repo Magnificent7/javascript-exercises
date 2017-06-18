bc5ca13c00728877cae4d504350340282834127f
dani <dani.zaghian@gmail.com>
2017-06-18 09:21:56 -0700

    js async examples

---
+++ js-async-examples/example.js
@@ -0,0 +1,12 @@
+// function firstTask(){
+//   console.log('First task done!');
+// }
+
+// function secondTask(){
+//   console.log('Second task done!');
+// }
+
+setTimeout(function(){
+  console.log('First task done!');
+}, 2000);
+console.log('Second task done!');
---
+++ js-async-examples/example.rb
@@ -0,0 +1,11 @@
+# def first_task
+#   puts 'First task done!'
+# end
+
+# def second_task
+#   puts 'Second task done!'
+# end
+
+sleep 2
+puts 'First task done'
+puts 'Second task done'
